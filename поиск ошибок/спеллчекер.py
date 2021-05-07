97099a2ea6c90492d07564a7bb63030a2cb0f547
etalesmmo <71773398+etalesmmo@users.noreply.github.com>
2021-05-07 18:42:19 +0300

    спеллчекер по нормальной форме
    прикручен pymorphy2, потому что spellcheker, судя по всему, не знает некоторые слова в косвенных падежах

--- поиск ошибок/спеллчекер.py
+++ поиск ошибок/спеллчекер.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import pymorphy2
 import re
 from spellchecker import SpellChecker
+
+morph = pymorphy2.MorphAnalyzer()
 
 spell = SpellChecker(language='ru')
 oneletter = ['а', 'в', 'и', 'к', 'о', 'с', 'у', 'я']
 # список количеств ошибок в каждом комментарии
 misspelled_list = []
 
-df = pd.read_csv(csvfile)
+df = pd.read_csv('сказки под грибом.csv')
 # список комментариев
 text = list(df.text) 
 
@@ -17,7 +20,9 @@ for line in text:
         line = re.findall('[а-яё]+', line, flags=re.IGNORECASE)
     except:
         line = []
-    wordlist = [word for word in line if word not in oneletter]    
-    misspelled = spell.unknown(wordlist)
+    wordlist = [morph.parse(word)[0] for word in line if word not in oneletter]
+    nf_list = [word.normal_form for word in wordlist]
+    misspelled = spell.unknown(nf_list)
     mis = len(misspelled)
     misspelled_list.append(mis)
+
